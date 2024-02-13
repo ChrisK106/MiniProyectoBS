e5a8527f30eee6bc3b2341dab7a6ebc957ab08e9
ChrisK106 <christian_k106@hotmail.com>
2024-02-13 05:27:08 -0500

    Limpieza de código y actualización de dependencias

---
+++ MiniProyectoBS/Models/Album.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniProyecto.Models
+{
+    public class Album
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
